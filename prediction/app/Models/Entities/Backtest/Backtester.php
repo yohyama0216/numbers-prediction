7d81f9766fd181ea7e8dab85f4d413de2537e8ef
大山陽平 <oyama_yohei@nanairo-inc.dev>
2023-01-19 21:33:33 +0900

    クラスを移動

--- prediction/app/Models/Entities/Backtest/Backtester.php
+++ prediction/app/Models/Entities/Backtest/Backtester.php
@@ -15,10 +15,7 @@ class Backtester {
 
     public function buySameDigitNumbers()
     {        
-        $numbers = range(111,999);
-        foreach($numbers as $number) {
-            $BuyNumbers3List[] = new BuyNumbers3(new Common\Numbers3($number),1);
-        }
+        $BuyNumbers3List = BuyNumbers3List::createSameDigitBuyNumbers3();
         $HitNumbers3ResultList = new HitNumbers3ResultList();
         foreach($this->DrawingNumbers3list as $DrawingNumbers3) {
             $HitNumbers3Result = HitChecker::check($DrawingNumbers3,$BuyNumbers3List);
@@ -27,19 +24,6 @@ class Backtester {
         return $HitNumbers3ResultList;
     }
 
-    /**
-     * 再頻出の数字だけを買う。（赤字）
-     */
-    public function buyFluentNumbers()
-    {        
-        $numbers = ['089'];
-        foreach($numbers as $number) {
-            $BuyNumbers3List[] = new BuyNumbers3(new Common\Numbers3($number),1);
-        }
-        $hitNumbers3ResultList = [];
-        foreach($this->DrawingNumbers3list as $DrawingNumbers3) {
-            $hitNumbers3ResultList[] = new HitNumbers3Result($DrawingNumbers3,$BuyNumbers3List);
-        }
-        return new HitNumbers3ResultList($hitNumbers3ResultList);
-    }
+    // 駄目だったケース
+    // 再頻出の数字だけを買う
 }
--- prediction/app/Models/Entities/Backtest/BuyNumbers3List.php
+++ prediction/app/Models/Entities/Backtest/BuyNumbers3List.php
@@ -3,13 +3,14 @@
 namespace App\Models\Entities\Backtest;
 
 use ArrayObject;
+use App\Models\Entities\Common;
 
 class BuyNumbers3List extends ArrayObject{
     private $Numbers = '';
     private $size = 1;
     private CONST YEN_PER_SIZE = 200;
 
-    public function __construct($Numbers,$size)
+    public function __construct($Numbers)
     {
         $this->Numbers = $Numbers;
     }
@@ -37,5 +38,14 @@ class BuyNumbers3List extends ArrayObject{
     public function calcCost()
     {
         return $this->size * self::YEN_PER_SIZE;
+    }
+
+    public static function createSameDigitBuyNumbers3()
+    {
+        $numbers = range(111,999);
+        foreach($numbers as $number) {
+            $BuyNumbers3List[] = new BuyNumbers3(new Common\Numbers3($number),1);
+        }
+        return $BuyNumbers3List;
     }
 }
