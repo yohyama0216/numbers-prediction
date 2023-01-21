a173d7b706e2ae5fcee3ef54a4ecc485fe4bf56f
大山陽平 <oyama_yohei@nanairo-inc.dev>
2023-01-21 23:23:43 +0900

    Pythonの機械学習予想を追加

---
+++ prediction/Python/Numbers3/Numbers3Predict.py
@@ -0,0 +1,31 @@
+import loadData as lD
+import buildData as bD
+import predictRandomForest as RF
+
+numbersData = []
+# index = 0
+# for p in range(0,4):
+#     numbersData.append([p,p**3,1])
+
+numbersData = lD.loadNumbers3Data()
+# print(numbersData)
+
+df = bD.build(numbersData,range(1,800),1,2401) # endで一ずれる？
+df.dropna(inplace=True)
+# print(df)
+X_columns = bD.createColumns('col',4,2400)
+y_columns = bD.createColumns('col',1,3)
+X = df[X_columns].values  # 説明変数
+y = df[y_columns].values     # 目的変数
+
+# print(X_columns)
+# print(y_columns)
+RF.predictRandomForestRegression(X,y)
+
+
+# データ100 / test_size=0.7 RMSE 学習: 1.08, テスト: 2.92 / R^2 学習: 0.86, テスト: -0.02
+# データ500 / test_size=0.7 RMSE 学習: 1.08, テスト: 2.90 / R^2 学習: 0.86, テスト: -0.01
+# データ800 / test_size=0.7 RMSE 学習: 1.09, テスト: 2.89 / R^2 学習: 0.86, テスト: -0.01
+# 上がらないので変える
+# estimatorを100から150に変えた
+# データ800 / test_size=0.7 RMSE 学習: 1.09, テスト: 2.87 / R^2 学習: 0.86, テスト: -0.01
