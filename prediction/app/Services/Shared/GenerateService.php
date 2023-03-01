95a50d5d172c8c16a3e584ed34d44770b2aa7144
大山陽平 <oyama_yohei@nanairo-inc.dev>
2023-03-01 20:58:20 +0900

    仮実装

--- prediction/app/Services/Shared/GenerateService.php
+++ prediction/app/Services/Shared/GenerateService.php
@@ -2,32 +2,28 @@
 
 namespace App\Services\Shared;
 
-use App\Repositories\DrawingResultRepository;
-
 class GenerateService
 {
-    private $DrawingResultRepository = null;
-
-    public function __construct(
-        DrawingResultRepository $DrawingResultRepository
-    )
-    {
-        $this->DrawingResultRepository = $DrawingResultRepository;
-    }
-
     /**
      * 同じ数字
      */
     public function getSameDigitNumbers($numbersType)
     {
-        return ["000","111","222","333","444","555","666","777","888","999"];
+        $list = [];
+        foreach(range(0,9) as $number) {
+            $list[] = str_repeat($number,$numbersType);
+        }
+        return $list;
     }
 
-    public function fromRecentFiveNumbers($resultCount)
+    /**
+     * 過去5回の数字から出ていない数字と出た数字の組み合わせを作る
+     */
+    public function fromRecentFiveNumbers($list)
     {
-        // todo 5回前の数字から予想するアルゴリズム　
-        // バックテストからも呼べるようにするか？　今のでOK?
-        // 色んな数字を作るアルゴリズムだけのクラスが必要。そうすればbacktestでも使える
+        $list = [111,123,456,887,991];
+        $list = array_unique(str_split(implode($list)));
+        // todo
         return ['111','333'];
     }
 }
